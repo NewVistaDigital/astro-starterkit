7d6d66c539d111d50b5531d18b79ec7bfd7e82f3
Jesse Mutzebaugh <jesse@newvistadigital.com>
2024-03-06 17:24:58 -0700

    feat: add optional components.ts, update global styles
    Add comment to consts.ts. Components.ts can be used as a single import on pages.

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+// N.B. THIS FILE IS IMPORTED IN BOTH SERVER- AND CLIENT-SIDE CODE.
+// THINK TWICE BEFORE ADDING STUFF AS IT WILL GET SHIPPED TO THE CLIENT.
+
 import { slugify } from './js/slugify';
 
 export const SITE_TITLE = 'NVD Astro Starterkit';
