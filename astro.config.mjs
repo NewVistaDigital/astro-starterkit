be00a81c233e0d1a7cad72a5d0960ecf925e5ebb
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-03-14 16:15:49 -0600

    Update to use tailwindcss. Move files around

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), icon()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), icon(), tailwind()]
 });
