46abdd9f42730fbe1988e14353b72e2a2c12ef72
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-03-14 22:03:15 -0600

    feat: add alert

--- src/consts.ts
+++ src/consts.ts
@@ -9,6 +9,7 @@ import { slugify } from './js/slugify';
 export const SITE_TITLE = 'NVD Astro Starterkit';
 export const SITE_DESCRIPTION = 'This is the default description of this website. Nice!';
 export const SITE_URL = 'https://example.com/';
+export const SITE_EMAIL = 'webmaster@newvistadigital.com';
 export const SITE_UTM_SOURCE = slugify(SITE_TITLE);
 export const SITE_DEFAULT_IMAGE = '/blog-placeholder-1.jpg';
 
