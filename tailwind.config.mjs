9aea930ed24b5002fae4f90bf31f4d8e21797dfe
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-03-21 10:23:49 -0600

    feat: require @tailwindcss/typography to use prose utilities

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
