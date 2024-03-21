fdd05b19497557f99795281fcee6717dff883099
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-03-21 10:21:28 -0600

    feat: add netlify, sitemap, and robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), icon(), tailwind()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), icon(), tailwind(), robotsTxt()],
+  output: "static",
+  adapter: netlify(),
 });
